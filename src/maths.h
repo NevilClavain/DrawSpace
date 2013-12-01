f5af413ae28ee855d8cb6490e8c9d79874b557bc
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2013-12-01 19:20:28 +0100

    maths utils added

---
+++ src/maths.h
@@ -0,0 +1,84 @@
+/*
+*                                                                          
+* DrawSpace Rendering engine                                               
+* Emmanuel Chaumont Copyright (c) 2013-2014                                
+*                                                                          
+* This file is part of DrawSpace.                                          
+*                                                                          
+*    DrawSpace is free software: you can redistribute it and/or modify     
+*    it under the terms of the GNU General Public License as published by  
+*    the Free Software Foundation, either version 3 of the License, or     
+*    (at your option) any later version.                                   
+*                                                                          
+*    DrawSpace is distributed in the hope that it will be useful,          
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of        
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         
+*    GNU General Public License for more details.                          
+*                                                                          
+*    You should have received a copy of the GNU General Public License     
+*    along with DrawSpace.  If not, see <http://www.gnu.org/licenses/>.    
+*                                                                          
+*/
+
+#ifndef _MATHS_H_
+#define _MATHS_H_
+
+#include <math.h>
+#include "vector.h"
+
+#ifndef PI
+#define PI 3.1415927
+#endif
+
+namespace DrawSpace
+{
+namespace Utils
+{
+class Maths
+{
+public:
+
+	static inline double	Min( dsreal a, dsreal b )					{ return (a < b ? a : b); };
+	static inline double	Max( dsreal a, dsreal b )					{ return (a > b ? a : b); };
+	
+	static inline double	Clamp( dsreal a, dsreal b, dsreal x )		{ return (x < a ? a : (x > b ? b : x)); };
+	static inline double	Lerp( dsreal a, dsreal b, dsreal x )		{ return a + x * (b - a); };
+	static inline double	Cubic( dsreal a )							{ return a * a * (3 - 2*a); };
+	
+	static inline double	DegToRad( dsreal ang )					    { return ( ( ang * PI ) / 180.0 ); };
+
+
+	static inline void		SphericaltoCartesian( const Vector& p_in, Vector& p_out )
+	{
+		p_out[2] = ( p_in[0] * cos( p_in[2] ) * cos( p_in[1] ) );
+		p_out[0] = ( p_in[0] * cos( p_in[2] ) * sin( p_in[1] ) );
+		p_out[1] = ( p_in[0] * sin( p_in[2] ) );
+	}
+	
+	static inline void		CartesiantoSpherical( Vector& p_in, Vector& p_out )
+	{
+		// cas particulier
+		if( p_in[1] > 0.0 && p_in[0] == 0.0 && p_in[2] == 0.0 )
+		{
+			p_out[0] = p_in[1];
+			p_out[2] = PI / 2.0;
+			p_out[1] = 0.0;
+			return;
+		}
+		else if( p_in[1] < 0.0 && p_in[0] == 0.0 && p_in[2] == 0.0 )
+		{
+			p_out[0] = -p_in[1];
+			p_out[2] = -PI / 2.0;
+			p_out[1] = 0.0;
+			return;
+		}
+		
+        p_out[0] = p_in.Length();
+		p_out[2] = asin( p_in[1] / p_out[0] );		
+		p_out[1] = atan2( p_in[0], p_in[2] );
+	}
+};
+}
+}
+
+#endif
