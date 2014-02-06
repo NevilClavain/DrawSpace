d0191742fc09bcd76dc027b44b4e6e802ffc32a1
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2014-02-06 13:32:07 +0100

    dynamics : World class added

--- src/world.h
+++ src/world.h
@@ -20,59 +20,24 @@
 *                                                                          
 */
 
-#ifndef _DRAWSPACE_H_
-#define _DRAWSPACE_H_
+#ifndef _WORLD_H_
+#define _WORLD_H_
 
-#include "tracedefs.h"
-#include "maths.h"
-#include "property.h"
-#include "task.h"
-#include "mutex.h"
-#include "parser.h"
-#include "vector.h"
-#include "matrix.h"
-#include "quaternion.h"
-#include "vertex.h"
-#include "triangle.h"
-#include "meshe.h"
-#include "archive.h"
-#include "file.h"
-#include "md5.h"
-#include "vsphere.h"
-#include "lod.h"
-#include "transformation.h"
-#include "timemanager.h"
-#include "transformation.h"
-#include "transformnode.h"
-#include "transformqueue.h"
-#include "renderingnode.h"
-#include "renderingqueue.h"
-#include "asset.h"
-#include "factory.h"
-#include "texture.h"
-#include "shader.h"
-#include "plugin.h"
-#include "pimanager.h"
-#include "renderstate.h"
-#include "renderer.h"
-#include "drawable.h"
-#include "fx.h"
-#include "viewportquad.h"
-#include "pass.h"
-#include "grbfile.h"
-#include "image.h"
-#include "font.h"
-#include "text.h"
-#include "text_widget.h"
-#include "scenegraph.h"
-#include "memalloc.h"
-#include "events.h"
-#include "camera.h"
-#include "fpsmovement.h"
-#include "freemovement.h"
-#include "quadtree.h"
-#include "misc_utils.h"
-#include "physic.h"
-#include "world.h"
-#include "inertbody.h"
+#include "drawspace_commons.h"
+
+namespace DrawSpace
+{
+namespace Dynamics
+{
+class World
+{
+protected:
+
+public:
+    World( void );
+    virtual ~World( void );
+
+};
+}
+}
 #endif
