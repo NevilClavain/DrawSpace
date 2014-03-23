dde8728489cdb68b5242bca066d310de8d070393
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2014-03-23 21:13:17 +0100

    rocket class added

--- src/rocket.cpp
+++ src/rocket.cpp
@@ -20,63 +20,17 @@
 *                                                                          
 */
 
-#ifndef _DRAWSPACE_H_
-#define _DRAWSPACE_H_
-
-#include "tracedefs.h"
-#include "exceptions.h"
-#include "maths.h"
-#include "property.h"
-#include "task.h"
-#include "mutex.h"
-#include "parser.h"
-#include "vector.h"
-#include "matrix.h"
-#include "quaternion.h"
-#include "vertex.h"
-#include "triangle.h"
-#include "meshe.h"
-#include "archive.h"
-#include "file.h"
-#include "md5.h"
-#include "vsphere.h"
-#include "lod.h"
-#include "transformation.h"
-#include "timemanager.h"
-#include "transformation.h"
-#include "transformnode.h"
-#include "transformqueue.h"
-#include "renderingnode.h"
-#include "renderingqueue.h"
-#include "asset.h"
-#include "factory.h"
-#include "texture.h"
-#include "shader.h"
-#include "plugin.h"
-#include "pimanager.h"
-#include "renderstate.h"
-#include "renderer.h"
-#include "drawable.h"
-#include "fx.h"
-#include "viewportquad.h"
-#include "pass.h"
-#include "grbfile.h"
-#include "image.h"
-#include "font.h"
-#include "text.h"
-#include "text_widget.h"
-#include "scenegraph.h"
-#include "memalloc.h"
-#include "events.h"
-#include "camera.h"
-#include "fpsmovement.h"
-#include "freemovement.h"
-#include "quadtree.h"
-#include "misc_utils.h"
-#include "physic.h"
-#include "world.h"
-#include "inertbody.h"
-#include "orbiter.h"
 #include "rocket.h"
-#include "calendar.h"
-#endif
+
+using namespace DrawSpace;
+using namespace DrawSpace::Core;
+using namespace DrawSpace::Utils;
+using namespace DrawSpace::Dynamics;
+
+Rocket::Rocket( World* p_world, DrawSpace::Interface::Drawable* p_drawable, const Parameters& p_parameters ) : Body( p_world, p_drawable )
+{
+}
+
+Rocket::~Rocket( void )
+{
+}
--- src/rocket.h
+++ src/rocket.h
@@ -20,63 +20,48 @@
 *                                                                          
 */
 
-#ifndef _DRAWSPACE_H_
-#define _DRAWSPACE_H_
+#ifndef _ROCKET_H_
+#define _ROCKET_H_
 
-#include "tracedefs.h"
-#include "exceptions.h"
-#include "maths.h"
-#include "property.h"
-#include "task.h"
-#include "mutex.h"
-#include "parser.h"
+#include "body.h"
 #include "vector.h"
 #include "matrix.h"
-#include "quaternion.h"
-#include "vertex.h"
-#include "triangle.h"
-#include "meshe.h"
-#include "archive.h"
-#include "file.h"
-#include "md5.h"
-#include "vsphere.h"
-#include "lod.h"
-#include "transformation.h"
-#include "timemanager.h"
-#include "transformation.h"
-#include "transformnode.h"
-#include "transformqueue.h"
-#include "renderingnode.h"
-#include "renderingqueue.h"
-#include "asset.h"
-#include "factory.h"
-#include "texture.h"
-#include "shader.h"
-#include "plugin.h"
-#include "pimanager.h"
-#include "renderstate.h"
-#include "renderer.h"
-#include "drawable.h"
-#include "fx.h"
-#include "viewportquad.h"
-#include "pass.h"
-#include "grbfile.h"
-#include "image.h"
-#include "font.h"
-#include "text.h"
-#include "text_widget.h"
-#include "scenegraph.h"
-#include "memalloc.h"
-#include "events.h"
-#include "camera.h"
-#include "fpsmovement.h"
-#include "freemovement.h"
-#include "quadtree.h"
-#include "misc_utils.h"
-#include "physic.h"
-#include "world.h"
-#include "inertbody.h"
-#include "orbiter.h"
-#include "rocket.h"
-#include "calendar.h"
+
+namespace DrawSpace
+{
+namespace Dynamics
+{
+class Rocket : public Body
+{
+public:
+
+    typedef struct
+    {       
+        DrawSpace::Utils::Vector    initial_pos;
+        DrawSpace::Utils::Matrix    initial_rot;
+
+        dsreal                      mass;
+
+        Body::ShapeDescr            shape_descr;
+
+    } Parameters;
+
+protected:
+
+    Parameters                      m_parameters;
+
+    btRigidBody*                    m_rigidBody;
+    btCollisionShape*               m_collisionShape;
+    btDefaultMotionState*           m_motionState;
+
+
+public:
+
+    Rocket( World* p_world, DrawSpace::Interface::Drawable* p_drawable, const Parameters& p_parameters );
+    virtual ~Rocket( void );
+    
+};
+}
+}
+
 #endif
