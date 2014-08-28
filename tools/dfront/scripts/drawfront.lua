2ed578645b65303cb00fc86e1d44caed07e85795
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2014-08-28 07:43:46 +0200

    dfront : scripts dir and dfront.lua added

---
+++ tools/dfront/scripts/drawfront.lua
@@ -0,0 +1,15 @@
+
+function globals()
+        for n,v in pairs(_G) do
+                print(n)
+                end
+        end
+
+renderframe = RenderFrame()
+default_scene = Scene()
+default_scene:SetName( "default" )
+renderframe:SetCurrentScene( default_scene:GetObject() )
+
+print( "--*- Welcome to DrawFront -*--" )
+print( "Lua version : " .. _VERSION )
+
