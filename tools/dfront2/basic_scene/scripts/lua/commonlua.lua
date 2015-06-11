1209f7b51834ce5ffc1d2b8660f49ec6f4740cbf
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2015-06-11 18:32:20 +0200

    basic_scene update
    commonlua script update

--- tools/dfront2/basic_scene/scripts/lua/commonlua.lua
+++ tools/dfront2/basic_scene/scripts/lua/commonlua.lua
@@ -1,4 +1,7 @@
 
+collisions_transforms = {}
+collisions_chunks = {}
+
 function globals()
         for n,v in pairs(_G) do
                 print(n)
@@ -42,11 +45,43 @@ function on_scenegraph_event( evt_type, scenegraph_name, node_alias )
       local shape_type = inert_body:GetShapeType()
       print( "shape type is " .. shape_type )
       
+      local collision_chunk_node_alias = node_alias .. "collisionchunk"
+      local collision_transform_node_alias = node_alias .. "collisiontransform"
+      
       if shape_type == 0 then        
         print( "shape box = " .. inert_body:GetShapeDescrBoxDimX() .. " " .. inert_body:GetShapeDescrBoxDimY() .. " " .. inert_body:GetShapeDescrBoxDimZ() )
+        
+        collisions_transforms[collision_transform_node_alias] = TransformationNode( collision_transform_node_alias, 1 )
+        collisions_transforms[collision_transform_node_alias]:LinkTo( scenegraph_name, node_alias )
+        local scale = Matrix()
+        scale:Scale( inert_body:GetShapeDescrBoxDimX() * 2, inert_body:GetShapeDescrBoxDimY() * 2, inert_body:GetShapeDescrBoxDimZ() * 2 )
+        collisions_transforms[collision_transform_node_alias]:UpdateMatrix( 0, scale )
+                
+        collisions_chunks[collision_chunk_node_alias] = ChunkNode( collision_chunk_node_alias )
+        collisions_chunks[collision_chunk_node_alias]:SetMesheName( "cube_meshe" )
+        collisions_chunks[collision_chunk_node_alias]:RegisterPassSlot( "texture_pass" )
+        collisions_chunks[collision_chunk_node_alias]:SetPassSlotFxName( "texture_pass", "wireframe_fx" )
+        collisions_chunks[collision_chunk_node_alias]:AddPassSlotShaderParam( "texture_pass", "color", 1, 0, Vector( 1.0, 0.0, 1.0, 1.0 ) )
+
+        collisions_chunks[collision_chunk_node_alias]:LinkTo( scenegraph_name, collision_transform_node_alias )        
           
       elseif shape_type == 1 then
         print( "shape sphere radius = " .. inert_body:GetShapeDescrSphereRadius() )
+        
+        collisions_transforms[collision_transform_node_alias] = TransformationNode( collision_transform_node_alias, 1 )
+        collisions_transforms[collision_transform_node_alias]:LinkTo( scenegraph_name, node_alias )
+        local scale = Matrix()
+        local radius = inert_body:GetShapeDescrSphereRadius();
+        scale:Scale( radius * 2, radius * 2, radius * 2 )
+        collisions_transforms[collision_transform_node_alias]:UpdateMatrix( 0, scale )
+        
+        collisions_chunks[collision_chunk_node_alias] = ChunkNode( collision_chunk_node_alias )
+        collisions_chunks[collision_chunk_node_alias]:SetMesheName( "sphere_meshe" )
+        collisions_chunks[collision_chunk_node_alias]:RegisterPassSlot( "texture_pass" )
+        collisions_chunks[collision_chunk_node_alias]:SetPassSlotFxName( "texture_pass", "wireframe_fx" )
+        collisions_chunks[collision_chunk_node_alias]:AddPassSlotShaderParam( "texture_pass", "color", 1, 0, Vector( 1.0, 0.0, 1.0, 1.0 ) )
+
+        collisions_chunks[collision_chunk_node_alias]:LinkTo( scenegraph_name, collision_transform_node_alias )        
         
       end
       
