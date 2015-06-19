c88b4f36e8ccfc1b2c3709a48a95d44e506a01e9
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2015-06-19 21:31:03 +0200

    basic_scene : update

--- tools/dfront2/basic_scene/scripts/lua/commonlua.lua
+++ tools/dfront2/basic_scene/scripts/lua/commonlua.lua
@@ -29,6 +29,54 @@ function print_matrix( m )
 	end
 end
 
+function create_collision_chunk( shape_type, collision_chunk_node_alias, collision_transform_node_alias, scenegraph_name, node_alias, bx, by, bz, radius )
+  
+  if shape_type == 0 then        
+        
+    collisions_transforms[collision_transform_node_alias] = TransformationNode( collision_transform_node_alias, 1 )
+    collisions_transforms[collision_transform_node_alias]:LinkTo( scenegraph_name, node_alias )
+    local scale = Matrix()
+    
+    scale:Scale( bx * 2, by * 2, bz * 2 )
+    collisions_transforms[collision_transform_node_alias]:UpdateMatrix( 0, scale )
+            
+    collisions_chunks[collision_chunk_node_alias] = ChunkNode( collision_chunk_node_alias )
+    collisions_chunks[collision_chunk_node_alias]:SetMesheName( "cube_meshe" )
+    
+    for i, pass_name in pairs( collisions_pass ) do
+    
+      collisions_chunks[collision_chunk_node_alias]:RegisterPassSlot( pass_name )
+      collisions_chunks[collision_chunk_node_alias]:SetPassSlotFxName( pass_name, "wireframe_fx" )
+      collisions_chunks[collision_chunk_node_alias]:AddPassSlotShaderParam( pass_name, "color", 1, 0, Vector( 1.0, 0.0, 1.0, 1.0 ) )
+    
+    end
+           
+    collisions_chunks[collision_chunk_node_alias]:LinkTo( scenegraph_name, collision_transform_node_alias )        
+      
+  elseif shape_type == 1 then
+    
+    collisions_transforms[collision_transform_node_alias] = TransformationNode( collision_transform_node_alias, 1 )
+    collisions_transforms[collision_transform_node_alias]:LinkTo( scenegraph_name, node_alias )
+    local scale = Matrix()
+    local radius = inert_body:GetShapeDescrSphereRadius();
+    scale:Scale( radius * 2, radius * 2, radius * 2 )
+    collisions_transforms[collision_transform_node_alias]:UpdateMatrix( 0, scale )
+    
+    collisions_chunks[collision_chunk_node_alias] = ChunkNode( collision_chunk_node_alias )
+    collisions_chunks[collision_chunk_node_alias]:SetMesheName( "sphere_meshe" )
+    
+    for i, pass_name in pairs( collisions_pass ) do
+    
+      collisions_chunks[collision_chunk_node_alias]:RegisterPassSlot( pass_name )
+      collisions_chunks[collision_chunk_node_alias]:SetPassSlotFxName( pass_name, "wireframe_fx" )
+      collisions_chunks[collision_chunk_node_alias]:AddPassSlotShaderParam( pass_name, "color", 1, 0, Vector( 1.0, 0.0, 1.0, 1.0 ) )
+      
+    end
+    collisions_chunks[collision_chunk_node_alias]:LinkTo( scenegraph_name, collision_transform_node_alias ) 
+    
+  end
+
+end
 
 print( "Lua version : " .. _VERSION )
 ds = DrawSpace()
@@ -53,51 +101,11 @@ ds:SetScenegraphNodeEventCallback( function( evt_type, scenegraph_name, node_ali
       local collision_chunk_node_alias = node_alias .. "collisionchunk"
       local collision_transform_node_alias = node_alias .. "collisiontransform"
       
-      if shape_type == 0 then        
-        print( "shape box = " .. inert_body:GetShapeDescrBoxDimX() .. " " .. inert_body:GetShapeDescrBoxDimY() .. " " .. inert_body:GetShapeDescrBoxDimZ() )
-        
-        collisions_transforms[collision_transform_node_alias] = TransformationNode( collision_transform_node_alias, 1 )
-        collisions_transforms[collision_transform_node_alias]:LinkTo( scenegraph_name, node_alias )
-        local scale = Matrix()
-        scale:Scale( inert_body:GetShapeDescrBoxDimX() * 2, inert_body:GetShapeDescrBoxDimY() * 2, inert_body:GetShapeDescrBoxDimZ() * 2 )
-        collisions_transforms[collision_transform_node_alias]:UpdateMatrix( 0, scale )
-                
-        collisions_chunks[collision_chunk_node_alias] = ChunkNode( collision_chunk_node_alias )
-        collisions_chunks[collision_chunk_node_alias]:SetMesheName( "cube_meshe" )
-        
-        for i, pass_name in pairs( collisions_pass ) do
-        
-          collisions_chunks[collision_chunk_node_alias]:RegisterPassSlot( pass_name )
-          collisions_chunks[collision_chunk_node_alias]:SetPassSlotFxName( pass_name, "wireframe_fx" )
-          collisions_chunks[collision_chunk_node_alias]:AddPassSlotShaderParam( pass_name, "color", 1, 0, Vector( 1.0, 0.0, 1.0, 1.0 ) )
-        
-        end
-               
-        collisions_chunks[collision_chunk_node_alias]:LinkTo( scenegraph_name, collision_transform_node_alias )        
-          
-      elseif shape_type == 1 then
-        print( "shape sphere radius = " .. inert_body:GetShapeDescrSphereRadius() )
-        
-        collisions_transforms[collision_transform_node_alias] = TransformationNode( collision_transform_node_alias, 1 )
-        collisions_transforms[collision_transform_node_alias]:LinkTo( scenegraph_name, node_alias )
-        local scale = Matrix()
-        local radius = inert_body:GetShapeDescrSphereRadius();
-        scale:Scale( radius * 2, radius * 2, radius * 2 )
-        collisions_transforms[collision_transform_node_alias]:UpdateMatrix( 0, scale )
-        
-        collisions_chunks[collision_chunk_node_alias] = ChunkNode( collision_chunk_node_alias )
-        collisions_chunks[collision_chunk_node_alias]:SetMesheName( "sphere_meshe" )
-        
-        for i, pass_name in pairs( collisions_pass ) do
-        
-          collisions_chunks[collision_chunk_node_alias]:RegisterPassSlot( pass_name )
-          collisions_chunks[collision_chunk_node_alias]:SetPassSlotFxName( pass_name, "wireframe_fx" )
-          collisions_chunks[collision_chunk_node_alias]:AddPassSlotShaderParam( pass_name, "color", 1, 0, Vector( 1.0, 0.0, 1.0, 1.0 ) )
-          
-        end
-
-        collisions_chunks[collision_chunk_node_alias]:LinkTo( scenegraph_name, collision_transform_node_alias )                
-      end      
+      create_collision_chunk( shape_type, collision_chunk_node_alias, collision_transform_node_alias, scenegraph_name, node_alias,
+                              inert_body:GetShapeDescrBoxDimX() * 2, 
+                              inert_body:GetShapeDescrBoxDimY() * 2, 
+                              inert_body:GetShapeDescrBoxDimZ() * 2,
+                              inert_body:GetShapeDescrSphereRadius() )     
     end    
   end
 end
