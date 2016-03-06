5c55bdcbf724878f07dd3d850490222e45e35841
krakoukass <manu95870@gmail.com>
2016-03-06 15:34:58 +0100

    update

--- tools/dfront2/basic_scene/scripts/shaders/map_height.hlsl
+++ tools/dfront2/basic_scene/scripts/shaders/map_height.hlsl
@@ -33,26 +33,27 @@ float ComputeVertexHeight( float4 p_vpos, float4 p_uv )
     /////////////////////////////////////////////////////////////////////////////// 
     float4 uv_mod = 0.0;
 
-    uv_mod.x = p_uv.x + ( fbm3 * 0.03 );
-    uv_mod.y = p_uv.y + ( -fbm3 * 0.03 );
+    uv_mod.x = p_uv.x + ( fbm3 * 0.015 );
+    uv_mod.y = p_uv.y + ( -fbm3 * 0.015 );
     
     float4 color = tex2Dlod( TexturePlanetMap, uv_mod );
     ////////////////////////////////////////////////////////////////////////////////////////
 
-    /*
-    float plain = ( fbm_params5.w * color.y );
-    res = plain;
-    */
+    
+    float plain = fbm_params5.w * color.y;
+    float mountains = fbm2 * fbm_params3.w + 1000.0;
 
+    
 	float pn = color.z;
 
-	float pn2 = 9.0 * pn; //( ( 0.5 * pn ) + 0.5 );
+	float pn2 = 9.0 * pn;
 	float weight = exp( pn2 ) / 1000.0;
 	
 
-	double hl = color.y; //clamp( fbm3, 0.0, 1.0 );
-	res = lerp( color.y * fbm_params5.w, color.y * fbm_params5.w + fbm2 * fbm_params3.w, hl * weight );
+	double hl = color.y;
+	res = lerp( plain, plain + mountains, hl * weight );
 	res += fbm_params.z;
+    
 
     return res;
 }
