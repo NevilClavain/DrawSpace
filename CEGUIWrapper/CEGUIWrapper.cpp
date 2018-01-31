bbacdf61cdd500504c8d3df00d17043c767b119a
NevilClavain <emmanuel.chaumont@wanadoo.f>
2018-01-31 21:09:57 +0100

    cleanup

--- CEGUIWrapper/CEGUIWrapper.cpp
+++ CEGUIWrapper/CEGUIWrapper.cpp
@@ -22,27 +22,6 @@
 */
 /* -*-LIC_END-*- */
 
-/*
-*                                                                          
-* DrawSpace Rendering engine                                               
-* Emmanuel Chaumont Copyright (c) 2013-2017                        
-*                                                                          
-* This file is part of DrawSpace.                                          
-*                                                                          
-*    DrawSpace is free software: you can redistribute it and/or modify     
-*    it under the terms of the GNU General Public License as published by  
-*    the Free Software Foundation, either version 3 of the License, or     
-*    (at your option) any later version.                                   
-*                                                                          
-*    DrawSpace is distributed in the hope that it will be useful,          
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of        
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         
-*    GNU General Public License for more details.                          
-*                                                                          
-*    You should have received a copy of the GNU General Public License     
-*    along with DrawSpace.  If not, see <http://www.gnu.org/licenses/>.    
-*
-*/
 
 #include "CEGUIWrapper.h"
 #include "exceptions.h"
@@ -281,7 +260,6 @@ void CEGUIWrapper::recurs_register_widgets( JSONParser& p_parser, int& p_token_i
                 {
                     recurs_register_widgets( p_parser, p_token_index, p_layout_path, widget_name );                
                 }
-
             }
         }
         else
