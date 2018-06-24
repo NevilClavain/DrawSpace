6fa90ae512a024d49f495d5715ab8a2ce041e504
NevilClavain <emmanuel.chaumont@wanadoo.f>
2018-06-24 12:54:44 +0200

    added generic_rendering.hlsl

--- shaders/hlsl_3_0/texture_mirror.vsh
+++ shaders/hlsl_3_0/texture_mirror.vsh
@@ -46,33 +46,7 @@ struct VS_OUTPUT
 };
 
 
-float4 reflectedVertexPos(float4 p_vertex, float4 p_reflectorPos, float4 p_reflectorNormale, float4x4 p_matWorld, float4x4 p_matView, float4x4 p_matProj)
-{
-    float4 rvp;
-
-    float4x4 id = 0;
-    id[0][0] = 1.0;
-    id[1][1] = 1.0;
-    id[2][2] = -1.0;
-    id[3][3] = 1.0;
-    float4x4 matView2 = mul(p_matView, id);
-    
-    float4 worldPos = mul(p_vertex, p_matWorld);
-    float4 worldPos2 = worldPos - p_reflectorPos;
-    worldPos2.w = 1.0;
-    
-    float4 worldPosRefl;
-    worldPosRefl.xyz = reflect(worldPos2.xyz, normalize(p_reflectorNormale.xyz));
-    worldPosRefl.w = worldPos.w;
-
-    float4 worldPos3 = worldPosRefl + p_reflectorPos;
-    worldPos3.w = 1.0;
-
-    float4 viewPos = mul(worldPos3, matView2);
-
-    rvp = mul(viewPos, p_matProj);
-    return rvp;
-}
+#include "generic_rendering.hlsl"
 
 VS_OUTPUT vs_main(VS_INPUT Input)
 {
--- shaders/hlsl_4_0/generic_rendering.hlsl
+++ shaders/hlsl_4_0/generic_rendering.hlsl
@@ -22,37 +22,30 @@
 */
 /* -*-LIC_END-*- */
 
-
-cbuffer legacyargs : register(b0)
+float4 reflectedVertexPos(float4 p_vertex, float4 p_reflectorPos, float4 p_reflectorNormale, float4x4 p_matWorld, float4x4 p_matView, float4x4 p_matProj)
 {
-    float4 vec[512];
-    Matrix mat[512];
-};
+    float4 rvp;
 
-#include "mat_input_constants.hlsl"
+    float4x4 id = 0;
+    id[0][0] = 1.0;
+    id[1][1] = 1.0;
+    id[2][2] = -1.0;
+    id[3][3] = 1.0;
+    float4x4 matView2 = mul(p_matView, id);
+    
+    float4 worldPos = mul(p_vertex, p_matWorld);
+    float4 worldPos2 = worldPos - p_reflectorPos;
+    worldPos2.w = 1.0;
+    
+    float4 worldPosRefl;
+    worldPosRefl.xyz = reflect(worldPos2.xyz, normalize(p_reflectorNormale.xyz));
+    worldPosRefl.w = worldPos.w;
 
-struct VS_INPUT
-{
-    float3 Position : POSITION;
-    float4 TexCoord0 : TEXCOORD0;
-};
+    float4 worldPos3 = worldPosRefl + p_reflectorPos;
+    worldPos3.w = 1.0;
 
-struct VS_OUTPUT
-{
-    float4 Position : SV_POSITION;
-    float2 TexCoord0 : TEXCOORD0;
-};
+    float4 viewPos = mul(worldPos3, matView2);
 
-#include "generic_rendering.hlsl"
-
-VS_OUTPUT vs_main(VS_INPUT Input)
-{
-    VS_OUTPUT Output;
-    float4 pos;
-    pos.xyz = Input.Position;
-    pos.w = 1.0;
-    Output.Position = reflectedVertexPos(pos, vec[24], vec[25], mat[matWorld], mat[matView], mat[matProj]);
-    Output.TexCoord0 = Input.TexCoord0.xy;
-      
-    return (Output);
+    rvp = mul(viewPos, p_matProj);
+    return rvp;
 }
