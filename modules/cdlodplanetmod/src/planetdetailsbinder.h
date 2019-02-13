6afc1995bee682d14130079ef200c2550011f03d
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2019-02-13 18:29:42 +0100

    planetdetailsbinder constness

--- modules/cdlodplanetmod/src/planetdetailsbinder.h
+++ modules/cdlodplanetmod/src/planetdetailsbinder.h
@@ -22,27 +22,6 @@
 */
 /* -*-LIC_END-*- */
 
-/*
-*                                                                          
-* DrawSpace Rendering engine                                               
-* Emmanuel Chaumont Copyright (c) 2013-2017                        
-*                                                                          
-* This file is part of DrawSpace.                                          
-*                                                                          
-*    DrawSpace is free software: you can redistribute it and/or modify     
-*    it under the terms of the GNU General Public License as published by  
-*    the Free Software Foundation, either version 3 of the License, or     
-*    (at your option) any later version.                                   
-*                                                                          
-*    DrawSpace is distributed in the hope that it will be useful,          
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of        
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         
-*    GNU General Public License for more details.                          
-*                                                                          
-*    You should have received a copy of the GNU General Public License     
-*    along with DrawSpace.  If not, see <http://www.gnu.org/licenses/>.    
-*
-*/
 
 #ifndef _PLANETDETAILSBINDER_H_
 #define _PLANETDETAILSBINDER_H_
@@ -136,13 +115,13 @@ public:
 		m_mirror_mode = p_state;
 	}
 
-	dsreal GetOceansDetailsAlt( void) { return m_ocean_details_alt; };
+	dsreal GetOceansDetailsAlt( void) const { return m_ocean_details_alt; };
 
-	PlanetLight GetLight( int p_index ) { return m_lights[p_index]; };
+	PlanetLight GetLight( int p_index ) const { return m_lights[p_index]; };
 
     void SetLandscapeBumpFactor( dsreal p_factor );
 
-    dsreal GetAtmoThickness( void ) { return m_atmoThickness; };
+    dsreal GetAtmoThickness( void ) const { return m_atmoThickness; };
 
     void EnableAtmoRender( bool p_value );
 
