7508f941647de35a62d277591eaad0de0b8f168d
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2020-08-05 21:23:21 +0200

    continent model added

---
+++ samples/lua/lua_models/continent/scripts_bank/continent_model.lua
@@ -0,0 +1,239 @@
+
+
+continent = {}
+
+continent.dump = {}
+continent.view = {}
+
+continent.view.lit = {}
+continent.view.wireframe = {}
+
+-- stockage des instances modeles : paire {entity, renderer}
+continent.models = {}
+
+
+continent.rendering_config = 
+{ 
+    lit_rendering = 
+	{
+		fx = 
+		{
+			shaders = 
+			{
+				{ path='lit_vs.cso',mode=SHADER_COMPILED },
+				{ path='lit_ps.cso',mode=SHADER_COMPILED }
+			},
+			rs_in = 
+			{
+				{ ope=RENDERSTATE_OPE_ENABLEZBUFFER, value="true" }
+			},
+			rs_out =
+			{
+				{ ope=RENDERSTATE_OPE_ENABLEZBUFFER, value="false" }
+			}
+		},
+		textures =
+		{
+			[1] = 
+			{
+				{ path='DefaultMaterial_albedo.jpg', stage=0 }
+			}
+		},
+		vertex_textures =
+		{
+		},
+		rendering_order = 10000,
+		shaders_params = 
+		{ 
+			{ param_name = "lights_enabled_v", shader_index = 0, register = 24 },
+			{ param_name = "light0_dir_v", shader_index = 0, register = 25 },
+			{ param_name = "flags_v", shader_index = 0, register = 26 },
+			{ param_name = "reflectorPos", shader_index = 0, register = 27 },
+			{ param_name = "reflectorNormale", shader_index = 0, register = 28 },
+			{ param_name = "ambient_color", shader_index = 1, register = 0 },
+			{ param_name = "lights_enabled", shader_index = 1, register = 1 },
+			{ param_name = "light0_color", shader_index = 1, register = 2 },
+			{ param_name = "light0_dir", shader_index = 1, register = 3 },
+			{ param_name = "flags", shader_index = 1, register = 7 },
+			{ param_name = "self_emissive", shader_index = 1, register = 8 },
+			{ param_name = "absorption", shader_index = 1, register = 9 },
+			{ param_name = "color", shader_index = 1, register = 10 },
+			{ param_name = "color_source", shader_index = 1, register = 11 },
+			{ param_name = "fog_color", shader_index = 1, register = 12 },
+			{ param_name = "flags2", shader_index = 1, register = 13 }
+		}
+	},
+	wireframe_rendering =
+	{
+		fx = 
+		{
+			shaders = 
+			{
+				{ path='color_vs.cso',mode=SHADER_COMPILED },
+				{ path='color_ps.cso',mode=SHADER_COMPILED }
+			},
+			rs_in = 
+			{
+				{ ope=RENDERSTATE_OPE_ENABLEZBUFFER, value="true" },
+				{ ope=RENDERSTATE_OPE_SETFILLMODE, value="line" }
+			},
+			rs_out =
+			{
+				{ ope=RENDERSTATE_OPE_ENABLEZBUFFER, value="false" },
+				{ ope=RENDERSTATE_OPE_SETFILLMODE, value="solid" }
+			}
+		},
+		textures =
+		{
+		},
+		vertex_textures =
+		{
+		},
+		rendering_order = 10000,
+		shaders_params = 
+		{ 
+		    { param_name = "color", shader_index = 1, register = 0 },
+		}	
+	},	
+	meshes_loader_params =
+	{
+		normale_generation_mode = NORMALES_AUTO_SMOOTH,
+		tb_generation_mode = TB_AUTO
+	},
+}
+
+continent.lit_material =
+{
+	color_source = { r = 1.0, g = 1.0, b = 1.0, a = 1.0 },
+	simple_color = { r = 1.0, g = 1.0, b = 1.0, a = 1.0 },
+	light_absorption = { r = 0.0, g = 0.0, b = 0.0, a = 0.0 },
+	self_emissive = { r = 0.0, g = 0.0, b = 0.0, a = 0.0 },
+}
+
+continent.dump.load = function()
+   continent.dump.entity = model.dump.load('continent.dump.entity','continent.ac')
+end
+
+continent.dump.unload = function()
+   continent.dump.entity = model.dump.unload(continent.dump.entity)
+   continent.dump.entity = nil;
+end
+
+continent.dump.show = function()
+   model.dump.show(continent.dump.entity)
+end
+
+continent.update_lit_from_scene_env = function( p_pass_id, p_environment_table, p_entity_id )
+
+    local renderer = continent.models[p_entity_id]['renderer']
+
+	renderer:set_shaderrealvector( p_pass_id, 'ambient_color', p_environment_table.ambient_light.r, p_environment_table.ambient_light.g, p_environment_table.ambient_light.b, p_environment_table.ambient_light.a )
+	renderer:set_shaderrealvector( p_pass_id, 'lights_enabled', p_environment_table.lights_enabled.x, p_environment_table.lights_enabled.y, p_environment_table.lights_enabled.z, p_environment_table.lights_enabled.w )
+	renderer:set_shaderrealvector( p_pass_id, 'light0_color', p_environment_table.light0.color.r, p_environment_table.light0.color.g, p_environment_table.light0.color.b, p_environment_table.light0.color.a )
+	renderer:set_shaderrealvector( p_pass_id, 'light0_dir', p_environment_table.light0.direction.x, p_environment_table.light0.direction.y, p_environment_table.light0.direction.z, p_environment_table.light0.direction.w )
+
+	renderer:set_shaderrealvector( p_pass_id, 'light0_dir_v', p_environment_table.light0.direction.x, p_environment_table.light0.direction.y, p_environment_table.light0.direction.z, p_environment_table.light0.direction.w )
+	renderer:set_shaderrealvector( p_pass_id, 'lights_enabled_v', p_environment_table.lights_enabled.x, p_environment_table.lights_enabled.y, p_environment_table.lights_enabled.z, p_environment_table.lights_enabled.w )
+	
+	renderer:set_shaderrealinvector( p_pass_id, 'flags_v', 0, p_environment_table.mirror)
+	renderer:set_shaderrealinvector( p_pass_id, 'flags_v', 1, p_environment_table.fog_intensity)
+	
+	renderer:set_shaderrealvector( p_pass_id, 'fog_color', p_environment_table.fog_color.r, p_environment_table.fog_color.g, p_environment_table.fog_color.b, 1.0 )
+		
+	renderer:set_shaderrealvector( p_pass_id, 'reflectorPos', p_environment_table.reflector_pos.x, p_environment_table.reflector_pos.y, p_environment_table.reflector_pos.z, 1.0 )
+	renderer:set_shaderrealvector( p_pass_id, 'reflectorNormale', p_environment_table.reflector_normale.x, p_environment_table.reflector_normale.y, p_environment_table.reflector_normale.z, 1.0 )
+
+	commons.apply_material( continent.lit_material, renderer, p_pass_id)
+
+	-- flag for bump mapping normales inputs
+	renderer:set_shaderrealvector( p_pass_id, 'flags2', 0.0, 0.0, 0.0, 0.0 )
+
+end
+
+continent.createlitmodelview = function(p_rendergraph, p_entitygraph, p_entity_id, p_passes_bindings, p_parent_entity_id)
+  
+  local entity
+  local renderer
+
+  entity, renderer = commons.create_rendered_meshe(continent.rendering_config, 'continent.ac', 'g TerrainMesh', p_passes_bindings)
+  renderer:register_to_rendering(p_rendergraph)
+
+  entity:add_aspect(BODY_ASPECT)
+  local body=Body()
+  body:attach_toentity(entity)
+  body:configure_shape(SHAPE_MESHE, 'continent.ac', 'g TerrainMesh')
+  body:configure_mode(COLLIDER_MODE)
+
+  p_entitygraph:add_child(p_parent_entity_id,p_entity_id,entity)
+
+  local pair = { ['entity'] = entity, ['renderer'] = renderer, ['body'] = body }
+
+  continent.models[p_entity_id] = pair
+
+  return entity
+end
+
+continent.trashmodelview = function(p_rendergraph, p_entitygraph, p_entity_id)
+
+  local entity = continent.models[p_entity_id]['entity']
+  local renderer = continent.models[p_entity_id]['renderer']
+
+  local body = continent.models[p_entity_id]['body']
+
+  body:release()
+  body:detach_fromentity(entity)
+
+  entity:remove_aspect(BODY_ASPECT)
+
+  commons.trash.meshe(p_rendergraph, entity, renderer)
+  p_entitygraph:remove(p_entity_id)
+
+  local pair = continent.models[p_entity_id]
+  pair['entity'] = nil
+  pair['renderer'] = nil
+  pair['body'] = nil
+
+  continent.models[p_entity_id] = nil
+end
+
+continent.models.dump = function()
+  for k, v in pairs(continent.models) do
+    g:print("continent entity instance -> "..k)
+  end  
+end
+
+continent.view.unload = function(p_entity_id)
+ 
+  local found_id = FALSE
+  for k, v in pairs(continent.models) do
+
+    if k == p_entity_id then
+	  found_id = TRUE
+	end
+  end
+
+  if found_id == TRUE then
+     model.view.unload(continent.trashmodelview,p_entity_id)
+  else
+    g:print('Unknown entity '..p_entity_id)
+  end
+end
+
+continent.view.load = function(p_entity_id, p_passes_config, p_parent_entity_id)
+
+  local found_id = FALSE
+  for k, v in pairs(continent.models) do
+
+    if k == p_entity_id then
+	  found_id = TRUE
+	end
+  end
+
+  if found_id == TRUE then
+    g:print('Entity '..p_entity_id..' already exists')
+  else
+    model.view.load('continent model', continent.createlitmodelview, p_passes_config, nil, nil, p_entity_id, p_parent_entity_id)
+  end
+end
+
+
--- samples/lua/lua_scenes/dinos_scene.lua
+++ samples/lua/lua_scenes/dinos_scene.lua
@@ -1,5 +1,39 @@
 
 	include('skydome_model.lua')
+	include('continent_model.lua')
+
+
+	max_apprun_cbfunc = function()
+
+      local normalized_light_dir = Vector(environment.light0.direction.x, environment.light0.direction.y, environment.light0.direction.z, 0.0)
+	  normalized_light_dir:normalize()
+
+	  local y = g:clamp(0.0, 1.0, -normalized_light_dir:get_y())	 
+	  model.env.fog.setdensity(y * 0.0005)
+
+	  local y0 = g:clamp(0.0, 1.0, 2.0 * normalized_light_dir:get_y())
+
+	  --[[
+	  raptor.lit_material.light_absorption.r = y0
+	  raptor.lit_material.light_absorption.g = y0
+	  raptor.lit_material.light_absorption.b = y0
+
+	  raptor.update_from_scene_env('texture_pass', environment, 'r')
+	  ]]
+
+    end
+	
+	continent_passes_config = 
+	{
+		texture_pass = 
+		{
+			rendering_id = 'lit_rendering',
+			lit_shader_update_func = continent.update_lit_from_scene_env
+		}
+	}	
+	continent.view.load('continent', continent_passes_config, 'root')
+
+	model.move.setpos('continent', 0.0, skydome.innerRadius, 0.0)
 	
 	skydome_passes_config = 
 	{
