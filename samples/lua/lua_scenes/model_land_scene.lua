a2da2b4bff169f47d0c7b66990cef3344a4ba57d
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2020-08-28 14:49:35 +0200

    land model added

---
+++ samples/lua/lua_scenes/model_land_scene.lua
@@ -0,0 +1,30 @@
+
+
+	
+	include('land_model.lua')
+
+	
+	land_passes_config = 
+	{
+		texture_pass = 
+		{
+			rendering_id = 'lit_rendering',
+			lit_shader_update_func = land.update_from_scene_env
+		}
+	}
+	land.view.load('l', {x = 0.0, y = 0.0, z = 0.0}, land_passes_config, 'root')
+	
+	
+	model.env.setbkcolor('texture_pass', 0.05,0.05,0.09)
+	
+	model.camera.mvt:set_pos(0.0, 8.0, 30.0)
+
+	model.env.light.setstate( TRUE )
+	
+	model.env.light.setsphericaldir(0.0,0.0)
+
+	model.env.ambientlight.setcolor(0.1, 0.1, 0.1)
+	
+	model.camera.mvt:set_pos(0.0, 8.0, 0.0)
+	model.camera.speed = 50.0
+	
