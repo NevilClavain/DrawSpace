e5fc374d5b4f22e13f8d4b920bd4dcb97b4dc855
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2020-08-29 14:17:19 +0200

    lua water sample refactoring WIP

--- samples/lua/water/waterluademo_assets/scripts_bank/water_i.lua
+++ samples/lua/water/waterluademo_assets/scripts_bank/water_i.lua
@@ -1,6 +1,7 @@
 
 
 include('land_model.lua')
+include('skydome_model.lua')
 
 
 hmi_mode=FALSE
@@ -64,7 +65,16 @@ text_renderer=TextRendering()
 text_renderer:configure(root_entity, "fps", 320, 30, 255, 0, 255, "??? fps")
 
 
-
+skydome_passes_config = 
+{
+	texture_pass = 
+	{
+		rendering_id = 'main_rendering',
+		lit_shader_update_func = skydome.update_from_scene_env
+	}
+}
+skydome.view.load('dome', skydome_passes_config, 'root')
+model.move.setpos('dome', 0.0, 0.0, 0.0)
 
 land_passes_config = 
 {
@@ -74,7 +84,7 @@ land_passes_config =
 		lit_shader_update_func = land.update_from_scene_env
 	}
 }
-land.view.load('l', {x = 0.0, y = 0.0, z = 0.0}, land_passes_config, 'root')
+land.view.load('l', {x = 0.0, y = skydome.innerRadius, z = 0.0}, land_passes_config, 'root')
 
 
 model.env.setbkcolor('texture_pass', 0.05,0.05,0.09)
@@ -83,11 +93,11 @@ model.camera.mvt:set_pos(0.0, 8.0, 30.0)
 
 model.env.light.setstate( TRUE )
 	
-model.env.light.setsphericaldir(0.0,0.0)
+model.env.light.setsphericaldir(0.0,20.0)
 
 model.env.ambientlight.setcolor(0.1, 0.1, 0.1)
 	
-model.camera.mvt:set_pos(0.0, 8.0, 0.0)
+model.camera.mvt:set_pos(0.0, skydome.innerRadius + 8.0, 0.0)
 model.camera.speed = 50.0
 
 
