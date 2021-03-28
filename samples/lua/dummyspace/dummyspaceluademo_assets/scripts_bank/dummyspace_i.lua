497d0aea1ef4203b2a817c12c5fe1e2c377b0b9e
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2021-03-28 12:01:31 +0200

    dummyspace lua demo added

---
+++ samples/lua/dummyspace/dummyspaceluademo_assets/scripts_bank/dummyspace_i.lua
@@ -0,0 +1,181 @@
+
+include('spacebox_model.lua')
+
+ctrl_key = FALSE
+
+resources_event = "..."
+
+g:add_resourceeventcb( "onresourceevent",
+function( event, resource_path, context )
+
+    local evt_out
+
+    if event == BLOB_LOAD then
+       evt_out = "loading :"..resource_path
+    elseif event == BLOB_LOADED then
+       evt_out = "loaded :"..resource_path
+    elseif event == ASSET_SETLOADEDBLOB then
+       evt_out = "set :"..resource_path
+    elseif event == SHADERCACHE_CREATION then
+       evt_out = "shader cache creation"
+    elseif event == SHADER_COMPILATION then
+       evt_out = "compiling :"..resource_path
+    elseif event == SHADER_COMPILED then
+       evt_out = "compilation done :"..resource_path
+    elseif event == ALL_ASSETS_LOADED then
+       evt_out = "All assets loaded !"
+       g:deactivate_resourcessystem();
+       rg:update_renderingqueues()
+    else
+       evt_out = "? : "..event       
+    end
+    resources_event = evt_out
+end)
+
+g:print('Current renderer is '..model.renderer_infos[1]..', '..model.renderer_infos[2]..'x'..model.renderer_infos[3])
+renderer_infos = {renderer:descr()}
+g:print('Current resolution is '..renderer_infos[2].." "..renderer_infos[3])
+
+
+mvt_mod = Module("mvtmod", "mvts")
+mvt_mod:load()
+g:print(mvt_mod:get_descr().. ' loaded')
+
+commons.init_final_pass(rg, 'final_pass')
+
+rg:create_child('final_pass', 'texture_pass', 0)
+rg:set_pass_targetclearstate( 'texture_pass', FALSE )
+rg:set_pass_depthclearstate( 'texture_pass', TRUE )
+
+
+model.createmainfreecamera(0.0, 0.0, 0.0, mvt_mod)
+eg:set_camera(model.camera.entity)
+
+
+camera_width, camera_height, zn, zf = model.camera.entity:read_cameraparams()
+
+g:print('camera params = '..camera_width..' '..camera_height..' '..zn..' '..zf )
+
+text_renderer=TextRendering()
+text_renderer:configure(root_entity, "resource_infos", 320, 130, 255, 0, 255, "resources...")
+
+container_angle_y_deg = 0.0
+container_angle_x_deg = 0.0
+
+
+g:activate_resourcessystem("init");
+
+mouse_right = FALSE
+
+
+g:add_mousemovecb( "onmousemove",
+function( xm, ym, dx, dy )  
+
+  if ctrl_key == TRUE then
+
+
+  else
+    local mvt_info = { model.camera.mvt:read() }
+    if mouse_right == FALSE then
+  	  model.camera.mvt:update(mvt_info[4],mvt_info[1],mvt_info[2],mvt_info[3],-dy / 4.0,-dx / 4.0, 0)
+    else
+	  model.camera.mvt:update(mvt_info[4],mvt_info[1],mvt_info[2],mvt_info[3],0,0,-dx)
+    end
+  end
+
+end)
+
+g:add_mouserightbuttondowncb( "onmouserightbuttondown", 
+function( xm, ym )
+  mouse_right = TRUE
+end)
+
+g:add_mouserightbuttonupcb( "onmouserightbuttonup", 
+function( xm, ym )
+  mouse_right = FALSE
+end)
+
+g:add_appruncb( "run",
+function()
+
+  text_renderer:update(10, 150, 255, 0, 0, resources_event)
+
+  local mvt_info = { model.camera.mvt:read() }
+  model.camera.mvt:update(mvt_info[4],mvt_info[1],mvt_info[2],mvt_info[3],0,0,0)
+
+end)
+
+g:add_keydowncb( "keydown",
+function( key )
+
+  --Q key
+  if key == 81 then 
+    local mvt_info = { model.camera.mvt:read() }
+
+	model.camera.mvt:update(5.0,mvt_info[1],mvt_info[2],mvt_info[3],0,0,0)
+      
+  --W key
+  elseif key == 87 then
+
+    local mvt_info = { model.camera.mvt:read() }
+
+	model.camera.mvt:update(-5.0,mvt_info[1],mvt_info[2],mvt_info[3],0,0,0)
+
+  elseif key == 17 then
+    ctrl_key = TRUE
+
+  end
+
+end)
+
+g:add_keyupcb( "keyup",
+function( key )  
+
+  last_key = key
+
+  --Q key
+  if key == 81 then
+    local mvt_info = { model.camera.mvt:read() }
+
+	model.camera.mvt:update(0.0,mvt_info[1],mvt_info[2],mvt_info[3],0,0,0)
+    
+  --W key
+  elseif key == 87 then
+    local mvt_info = { model.camera.mvt:read() }
+
+	model.camera.mvt:update(0.0,mvt_info[1],mvt_info[2],mvt_info[3],0,0,0)
+
+  elseif key == 17 then
+    ctrl_key = FALSE
+     
+  end
+end)
+
+
+g:show_mousecursor(FALSE)
+g:set_mousecursorcircularmode(TRUE)
+
+g:signal_renderscenebegin("eg")
+
+spacebox_passes_bindings = 
+{
+	binding_0 = 
+	{
+        target_pass_id = 'texture_pass',
+		rendering_id = 'spacebox_rendering',
+		lit_shader_update_func = nil
+	}
+}
+spaceboxmod.view.load('spacebox0', spacebox_passes_bindings)
+model.setup_rawtransformationschain(spaceboxmod.models['spacebox0'].entity, 'spacebox0', {x = 1000.0, y = 1000.0, z = 1000.0 } )
+eg:add_child('root', 'spacebox0', spaceboxmod.models['spacebox0'].entity)
+
+model.env.setbkcolor('texture_pass', 0.0,0.0,0.0)
+
+model.camera.mvt:set_pos(0.0, 0.0, 10.0)
+
+
+model.env.light.setstate( TRUE )
+model.env.light.setdir(1.0, -0.4, 0.0)
+model.env.ambientlight.setcolor(0.1, 0.1, 0.1)
+
---
+++ samples/lua/dummyspace/dummyspaceluademo_assets/scripts_bank/dummyspace_r.lua
@@ -0,0 +1,27 @@
+
+
+g:signal_rendersceneend("eg")
+
+
+spaceboxmod.view.unload('spacebox0')
+
+model.releasemaincamera()
+
+g:remove_appruncb('run')
+
+
+rg:remove_pass('texture_pass')
+
+
+commons.trash.final_pass(rg, 'final_pass')
+
+text_renderer:release()
+
+rg:cleanup_renderingqueues()
+g:release_assets()
+
+
+g:show_mousecursor(TRUE)
+g:set_mousecursorcircularmode(FALSE)
+
+mvt_mod:unload()
