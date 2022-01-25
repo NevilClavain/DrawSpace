6e3667cd56a6d25eb1ef6b05b0d111e0e7796616
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2022-01-25 09:31:21 +0100

    mesh lua extension WIP

---
+++ lua_extensions/mesh_luaext/src/meshluaext.cpp
@@ -0,0 +1,41 @@
+/* -*-LIC_BEGIN-*- */
+/*
+*
+* DrawSpace Rendering engine
+* Emmanuel Chaumont Copyright (c) 2013-2022
+*
+* This file is part of DrawSpace.
+*
+*    DrawSpace is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    DrawSpace is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with DrawSpace.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+/* -*-LIC_END-*- */
+
+#include "meshluaext.h"
+//#include "luaclass_stringrendering.h"
+#include "plugin.h"
+
+MeshLuaExtension::MeshLuaExtension(void)
+{
+}
+
+void MeshLuaExtension::Register(lua_State* p_L)
+{
+	//Luna<LuaClass_StringsRendering>::Register(p_L);
+}
+
+void MeshLuaExtension::UpdateRenderer(DrawSpace::Interface::Renderer* p_renderer)
+{
+	DrawSpace::Core::SingletonPlugin<DrawSpace::Interface::Renderer>::GetInstance()->m_interface = p_renderer;
+}
---
+++ lua_extensions/mesh_luaext/src/meshluaext.h
@@ -0,0 +1,40 @@
+/* -*-LIC_BEGIN-*- */
+/*
+*
+* DrawSpace Rendering engine
+* Emmanuel Chaumont Copyright (c) 2013-2022
+*
+* This file is part of DrawSpace.
+*
+*    DrawSpace is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    DrawSpace is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with DrawSpace.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+/* -*-LIC_END-*- */
+
+#pragma once
+
+#include "luaext.h"
+
+class MeshLuaExtension : public LuaExtension
+{
+public:
+	MeshLuaExtension(void);
+
+	void Register(lua_State* p_L);
+	void UpdateRenderer(DrawSpace::Interface::Renderer* p_renderer);
+
+	__LUAEXT_DESCRIPTION__("Strings and text rendering")
+};
+
+
