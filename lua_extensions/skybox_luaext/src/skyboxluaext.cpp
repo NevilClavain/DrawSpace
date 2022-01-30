cc4d56e456e4d25e81e41a37ae39cb63d0d75d2c
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2022-01-30 12:47:49 +0100

    added skybox lua ext extension

---
+++ lua_extensions/skybox_luaext/src/skyboxluaext.cpp
@@ -0,0 +1,41 @@
+/* -*-LIC_BEGIN-*- */
+/*
+*
+* DrawSpace Rendering engine
+* Emmanuel Chaumont Copyright (c) 2013-2022
+*
+* This file is part of DrawSpace.
+*
+*    DrawSpace is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    DrawSpace is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with DrawSpace.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+/* -*-LIC_END-*- */
+
+#include "skyboxluaext.h"
+//#include "luaclass_skyboxrendering.h"
+#include "plugin.h"
+
+SkyboxLuaExtension::SkyboxLuaExtension(void)
+{
+}
+
+void SkyboxLuaExtension::Register(lua_State* p_L)
+{
+	//Luna<LuaClass_SkyboxRendering>::Register(p_L);
+}
+
+void SkyboxLuaExtension::UpdateRenderer(DrawSpace::Interface::Renderer* p_renderer)
+{
+	DrawSpace::Core::SingletonPlugin<DrawSpace::Interface::Renderer>::GetInstance()->m_interface = p_renderer;
+}
