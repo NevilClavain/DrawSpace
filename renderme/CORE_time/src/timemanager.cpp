fc5b5cf49fee5ef15e9b59a125300c5a7697d5a9
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2023-11-05 19:09:30 +0100

    added CORE_time and time manager WIP

---
+++ renderme/CORE_time/src/timemanager.cpp
@@ -0,0 +1,188 @@
+/* -*-LIC_BEGIN-*- */
+/*
+*
+* renderMe grafx framework
+* Emmanuel Chaumont Copyright (c) 2013-2023
+*
+* This file is part of renderMe.
+*
+*    renderMe is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    renderMe is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with renderMe.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+/* -*-LIC_END-*- */
+
+#include <windows.h>
+#include <cmath>
+
+#include "timemanager.h"
+
+using namespace renderMe::core;
+
+TimeManager::TimeManager()
+{
+}
+
+void TimeManager::reset(void)
+{
+    m_last_tick = 0;
+    m_current_tick = 0;
+    m_fps = 0;
+    m_frame_count = 0;
+    m_ready = false;
+    m_last_deltatime = 0;
+}
+
+void TimeManager::update(void)
+{
+    const auto current_tick{ ::GetTickCount() };
+
+    if (m_last_tick)
+    {
+        if (current_tick - m_last_tick >= 1000)
+        {
+            m_last_deltatime = current_tick - m_last_tick;
+            m_last_tick = current_tick;
+            m_fps = m_frame_count;
+            m_frame_count = 0;
+
+            if (m_fps > 1)
+            {
+                // declare ready only if we get a decent fps
+                m_ready = true;
+            }
+        }
+    }
+    else
+    {
+        m_last_tick = current_tick;
+    }
+    m_frame_count++;
+
+    if (m_ready)
+    {
+        // TODO later
+        /*
+        // timers management
+        for (auto it = m_timers.begin(); it != m_timers.end(); ++it)
+        {
+            const auto timer{ (*it) };
+
+            if (timer->m_state && !timer->m_freeze)
+            {
+                if (-1 == timer->m_prev_tick)
+                {
+                    timer->m_prev_tick = current_tick;
+                }
+                else
+                {
+                    timer->m_tick_count += current_tick - timer->m_prev_tick;
+                    if (timer->m_tick_count >= timer->m_period)
+                    {
+                        timer->m_tick_count = 0;
+                        timer->expired();
+                    }
+
+                    timer->m_prev_tick = current_tick;
+                }
+            }
+        }
+        */
+        m_current_tick = current_tick;
+    }
+}
+
+
+bool TimeManager::isReady(void) const
+{
+    return m_ready;
+}
+
+long TimeManager::getLastDeltaTime(void) const
+{
+    return m_last_deltatime;
+}
+
+long TimeManager::getCurrentTick(void) const
+{
+    return m_current_tick;
+}
+
+long TimeManager::getFPS(void) const
+{
+    return m_fps;
+}
+
+double TimeManager::convertUnitPerSecFramePerSec(double p_speed)
+{
+    if (!m_ready)
+    {
+        return 0.0;
+    }
+    return (p_speed / m_fps);
+}
+
+void TimeManager::angleSpeedInc(double* p_angle, double p_angleSpeed)
+{
+    if (!m_ready) return;
+
+    // on veut, a partir de la vitesse en degres/s fixee, trouver
+    // la vitesse en degres / frame -> on fait donc (deg/sec)/(frame/sec) 
+    const double angleSpeedDegPerFrame{ p_angleSpeed / m_fps };
+    double angle{ *p_angle };
+
+    angle += angleSpeedDegPerFrame;
+    angle = std::fmod(angle, 360.0);
+
+    *p_angle = angle;
+}
+
+void TimeManager::angleSpeedDec(double* p_angle, double p_angleSpeed)
+{
+    if (!m_ready) return;
+
+    // on veut, a partir de la vitesse en degres/s fixee, trouver
+    // la vitesse en degres / frame -> on fait donc (deg/sec)/(frame/sec) 
+    const double angleSpeedDegPerFrame{ p_angleSpeed / m_fps };
+    double angle{ *p_angle };
+
+    angle -= angleSpeedDegPerFrame;
+    angle = std::fmod(angle, 360.0);
+
+    if (*p_angle <= 0.0f)
+    {
+        angle = 360.0 + angle;
+    }
+
+    *p_angle = angle;
+}
+
+void TimeManager::translationSpeedInc(double* p_translation, double p_speed)
+{
+    if (!m_ready) return;
+
+    // on veut, a partir de la vitesse en unites/s fixee, trouver
+    // la vitesse en unite / frame -> on fait donc (unit/sec)/(frame/sec)
+    const double translationSpeedUnitPerFrame{ p_speed / m_fps };
+    *p_translation += translationSpeedUnitPerFrame;
+}
+
+void TimeManager::translationSpeedDec(double* p_translation, double p_speed)
+{
+    if (!m_ready) return;
+
+    // on veut, a partir de la vitesse en unites/s fixee, trouver
+    // la vitesse en unite / frame -> on fait donc (unit/sec)/(frame/sec)
+    const double translationSpeedUnitPerFrame{ p_speed / m_fps };
+    *p_translation -= translationSpeedUnitPerFrame;
+}
---
+++ renderme/CORE_time/src/timemanager.h
@@ -0,0 +1,64 @@
+/* -*-LIC_BEGIN-*- */
+/*
+*
+* renderMe grafx framework
+* Emmanuel Chaumont Copyright (c) 2013-2023
+*
+* This file is part of renderMe.
+*
+*    renderMe is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    renderMe is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with renderMe.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+/* -*-LIC_END-*- */
+
+#pragma once
+
+namespace renderMe
+{
+	namespace core
+	{
+        class TimeManager
+        {
+        public:
+            TimeManager(void);
+            ~TimeManager(void) = default;
+
+            void    reset(void);
+
+            void    update(void);
+
+            void    angleSpeedInc(double* p_angle, double p_angleSpeed);
+            void    angleSpeedDec(double* p_angle, double p_angleSpeed);
+            void    translationSpeedInc(double* p_translation, double p_speed);
+            void    translationSpeedDec(double* p_translation, double p_speed);
+
+            double  convertUnitPerSecFramePerSec(double p_speed);
+
+            bool    isReady(void) const;
+            long    getLastDeltaTime(void) const;
+            long    getCurrentTick(void) const;
+            long    getFPS(void) const;
+
+        private:
+            long    m_last_tick{ 0 };
+            long    m_frame_count{ 0 };
+            long    m_fps{ 0 };
+            bool    m_ready{ false };
+            long    m_last_deltatime{ 0 };
+            long    m_current_tick{ 0 };
+
+        };
+
+	} // core
+} //renderMe
