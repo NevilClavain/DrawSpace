3af539788ab26b20a482c7a8519f04d15cd595fa
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2023-11-09 23:00:49 +0100

    clean

--- renderme/commons/exceptions.h
+++ renderme/commons/exceptions.h
@@ -2,23 +2,23 @@
 /* -*-LIC_BEGIN-*- */
 /*
 *
-* DrawSpace Rendering engine
+* renderMe grafx framework
 * Emmanuel Chaumont Copyright (c) 2013-2023
 *
-* This file is part of DrawSpace.
+* This file is part of renderMe.
 *
-*    DrawSpace is free software: you can redistribute it and/or modify
+*    renderMe is free software: you can redistribute it and/or modify
 *    it under the terms of the GNU General Public License as published by
 *    the Free Software Foundation, either version 3 of the License, or
 *    (at your option) any later version.
 *
-*    DrawSpace is distributed in the hope that it will be useful,
+*    renderMe is distributed in the hope that it will be useful,
 *    but WITHOUT ANY WARRANTY; without even the implied warranty of
 *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *    GNU General Public License for more details.
 *
 *    You should have received a copy of the GNU General Public License
-*    along with DrawSpace.  If not, see <http://www.gnu.org/licenses/>.
+*    along with renderMe.  If not, see <http://www.gnu.org/licenses/>.
 *
 */
 /* -*-LIC_END-*- */
